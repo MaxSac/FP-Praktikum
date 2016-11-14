853cfd2c34bb6e0278ec78d1321f450f023c817f
Philip Schäfers <phil.schaefers@gmail.com>
2016-11-14 13:25:44 +0100

    Daten eingetragen

--- 01-14.11.2016/plot.py
+++ 01-14.11.2016/plot.py
@@ -11,30 +11,30 @@ def stab(r1,r2,L):
     return (1-L/r1)*(1-L/r2)
 
 r1 = 1000000; r2 = 1000000;
-if r1<r2 :a = r1
-elif r1>r2: a = r2 
+if r1<r2 : a = r1
+elif r1>r2: a = r2
 else: a = 2 * r1
 L = np.linspace(0,a,100)
 plt.plot(L ,stab(r1, r2, L),label='$r_1 = flat , r_2 = flat $')
 
 
 r1 = 100000000; r2 = 1.4;
-if r1<r2 :a = r1
-elif r1>r2: a = r2 
+if r1<r2 : a = r1
+elif r1>r2: a = r2
 else: a = 2*r1
 L = np.linspace(0,a,100)
 plt.plot(L ,stab(r1, r2, L),label='$r_1 = flat, r_2 = 1.4 m$')
 
 
-r1 = 100000000; r2 = 1.0;
-if r1<r2 :a = r1
-elif r1>r2: a = r2 
+r1 = 1.5; r2 = 1.0;
+if r1<r2 : a = r1
+elif r1>r2: a = r2
 else: a = 2*r1
 L = np.linspace(0,a,100)
-plt.plot(L ,stab(r1, r2, L),label='$r_1 = flat, r_2 = 1.0 m$')
+plt.plot(L ,stab(r1, r2, L),label='$r_1 = 2.0, r_2 = 1.0 m$')
 
 plt.grid()
-plt.xlim(xmax=2)
+plt.xlim(xmin=0, xmax=2)
 plt.ylim(ymax=1.1)
 plt.xlabel('Resonatorlänge / m')
 plt.ylabel('Stabilitätsparameter')
