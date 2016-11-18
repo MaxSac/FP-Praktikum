bd33d7ee2bdda3e8bcb9d80b2e6d1bd91b2768b4
MaxSac <maximilian.sackel@tu-dortmund.de>
2016-11-18 19:17:23 +0100

    auswerung beginn

--- 01-14.11.2016/plot.py
+++ 01-14.11.2016/plot.py
@@ -10,58 +10,67 @@ from lmfit import minimize, Parameter, Model
 def stab(r1,r2,L):
     return (1-L/r1)*(1-L/r2)
 
-r1 = 1000000; r2 = 1000000;
+r1 = 1000000; r2 = 1.4;
 if r1<r2 : a = r1
 elif r1>r2: a = r2
 else: a = 2 * r1
 L = np.linspace(0,a,100)
-plt.plot(L ,stab(r1, r2, L),label='$r_1 = flat , r_2 = flat $')
+plt.plot(L ,stab(r1, r2, L),label=r'$r_1 = \, \text{flat} , r_2 = 1.4 \, \text{m} $')
 
 
-r1 = 100000000; r2 = 1.4;
+r1 = 1.4; r2 = 1.4;
 if r1<r2 : a = r1
 elif r1>r2: a = r2
 else: a = 2*r1
 L = np.linspace(0,a,100)
-plt.plot(L ,stab(r1, r2, L),label='$r_1 = flat, r_2 = 1.4 m$')
-
-
-r1 = 1.5; r2 = 1.0;
-if r1<r2 : a = r1
-elif r1>r2: a = r2
-else: a = 2*r1
-L = np.linspace(0,a,100)
-plt.plot(L ,stab(r1, r2, L),label='$r_1 = 2.0, r_2 = 1.0 m$')
+plt.plot(L ,stab(r1, r2, L),label=r'$r_1 = \, \text{flat}, r_2 = 1.4 \, \text{m}$')
 
 plt.grid()
-plt.xlim(xmin=0, xmax=2)
-plt.ylim(ymax=1.1)
+plt.xlim(xmin=0, xmax=2.8)
+plt.ylim(ymax=1)
 plt.xlabel('Resonatorlänge / m')
 plt.ylabel('Stabilitätsparameter')
 plt.legend(loc='best')
 plt.savefig('build/Stabilisationsparameter.pdf')
 plt.close()
 
+#Intensität in Abhängikeit der Polarisation
 winkel, intensi = np.loadtxt('Polarisation.txt', unpack=True)
 winkel = np.radians(winkel)
 def fsin(x, a, b, c, d):
-    return np.sin(b*x +c)*a +d 
+    return (np.sin(b*x +c)**2)*a +d 
 
-params, cov = curve_fit(fsin, winkel, intensi, bounds=([80,2,1.3,70],[150,4,10,150]))
-plt.plot(winkel, intensi, 'x')
-plt.plot(winkel, fsin(winkel, *params))
+params, cov = curve_fit(fsin, winkel, intensi,
+        bounds=([150,0.5,0,0],[250,1.5,2*np.pi,50]))
+linw = np.linspace(0,max(winkel),100)
+plt.plot(winkel, intensi, 'x', label='Messwerte')
+plt.plot(linw , fsin(linw, *params), label='Fit')
+plt.legend(loc='best')
 plt.savefig('build/Polar.pdf')
 plt.close()
 
+#Intensität der verschiedenen TEM Moden
 A, TEM00, TEM10 = np.loadtxt('TEM-Moden.txt', unpack=True)
+
 def gausian(x, a, v, my):
     return a*np.exp(-(x+my)**2/(v**2))
 params, cov = curve_fit(gausian, A, TEM00, bounds=([3,5,-20],[6,10,-10]))
-print(params)
-x = np.linspace(0,35,100)
+
+def gausian2( x, a, v, my):
+    return a*8*(x-my)**2/v**2*np.exp(-0.5*(x-my)**2/v**2)
+params2, cov2 = curve_fit(gausian2, A, TEM10,bounds=([0,0,12],[1,10,20])) 
+
+x = np.linspace(0,max(A+1),100)
 plt.plot(x, gausian(x, *params))
 plt.plot(A, TEM00, 'x', label='TEM00')
-plt.plot(A, TEM10, 'x', label='TEM10')
+plt.xlim(xmin=0, xmax=max(A+1))
 plt.legend(loc='best')
-plt.savefig('build/Modenblende.pdf')
+plt.savefig('build/TEM00.pdf')
+plt.close()
+
+plt.plot(x, gausian2(x, *params2))
+plt.plot(A, TEM10, 'x', label='TEM10')
+plt.xlim(xmin=0, xmax=max(A+1))
+plt.legend(loc='best')
+plt.savefig('build/TEM10.pdf')
 plt.close()
